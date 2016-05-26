29a4e90944a61ff4235938e6037e441c1317ec0e
LinKen <team2014@163.com>
2016-05-26 10:08:25 +0800

    test
    test

---
+++ app/src/main/java/com/eva/httpdemo/test/Test.java
@@ -0,0 +1,25 @@
+package com.eva.httpdemo.test;
+
+/**
+ * Created by linken on 2016-05-26.
+ */
+public class Test {
+    String name ;
+    int age ;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
